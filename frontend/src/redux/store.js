c60ce38d54348cde2ede00adb20b7e5c4b19ad0f
Lewis Sellers <lasellers@gmail.com>
2020-08-21 16:43:45 -0400

    Add chrome redux extension support

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,5 +1,7 @@
-import { createStore } from "redux";
+import {createStore} from "redux";
 
 import rootReducer from "./reducers/index";
 
-export default createStore(rootReducer);
+export default createStore(rootReducer,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
